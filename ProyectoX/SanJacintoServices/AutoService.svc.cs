7a5a5bc0a015a376684bc0db60227e11d6b742f9
ronny.ims <ronny.ims@520c7223-f99b-752e-b2e1-b03ac67e0b09>
2014-05-20 08:21:03 +0000

    Creacion de listados marcas y modelos

---
+++ ProyectoX/SanJacintoServices/AutoService.svc.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using SanJacintoServices.Dominio;
+using SanJacintoServices.Persistencia;
+
+namespace SanJacintoServices
+{
+    
+    public class AutoService : IAutoService
+    {
+        private AutoDAO autoDAO = null;
+        private AutoDAO AutoDAO
+        {
+            get
+            {
+                if (autoDAO == null)
+                    autoDAO = new AutoDAO();
+                return autoDAO;
+            }
+        }
+
+        private MarcaDAO marcaDAO = null;
+        private MarcaDAO MarcaDAO
+        {
+            get
+            {
+                if (marcaDAO == null)
+                    marcaDAO = new MarcaDAO();
+                return marcaDAO;
+            }
+        }
+
+        private ModeloDAO modeloDAO = null;
+        private ModeloDAO ModeloDAO
+        {
+            get
+            {
+                if (modeloDAO == null)
+                    modeloDAO = new ModeloDAO();
+                return modeloDAO;
+            }
+        }
+
+        public List<Marca> listarMarcas()
+        {
+            return MarcaDAO.ListarTodos().ToList();
+        }
+
+
+        public List<Modelo> listarModelos()
+        {
+            return ModeloDAO.ListarTodos().ToList();
+        }
+    }
+}
---
+++ ProyectoX/SanJacintoServices/IAutoService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using SanJacintoServices.Dominio;
+
+namespace SanJacintoServices
+{
+    
+    [ServiceContract]
+    public interface IAutoService
+    {
+        [OperationContract]
+        List<Marca> listarMarcas();
+
+        [OperationContract]
+        List<Modelo> listarModelos();
+    }
+}
