91190bd698c94065596a55aa8e730e95c4aa6f24
fbngeldres@gmail.com <fbngeldres@gmail.com@520c7223-f99b-752e-b2e1-b03ac67e0b09>
2014-05-26 03:54:19 +0000

    Test Auto

---
+++ ProyectoX/SanJacintoTest/AutoTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SanJacintoTest
+{
+    [TestClass]
+    public class AutoTest
+    {
+        [TestMethod]
+        public void ListarResutadoAutosTest()
+        {
+            wsAutoService.AutoServiceClient proxy = new wsAutoService.AutoServiceClient();
+            List<wsAutoService.Auto> list;
+
+            list = proxy.listarResultadoAutos(0, 0, 0, 0, 0).ToList();
+            Assert.IsNotNull(list);
+            System.Diagnostics.Debug.WriteLine("" + list.Count);
+            Assert.IsTrue(list.Count > 0);
+        }
+
+        [TestMethod]
+        public void ListarResutadoEntrePrecioTest()
+        {
+            wsAutoService.AutoServiceClient proxy = new wsAutoService.AutoServiceClient();
+            List<wsAutoService.Auto> list;
+
+            list = proxy.listarResultadoAutos(0, 0, 11, 13, 0).ToList();
+            Assert.IsNotNull(list);
+            System.Diagnostics.Debug.WriteLine("" + list.Count);
+            Assert.IsTrue(list.Count == 1);
+        }
+    }
+}
