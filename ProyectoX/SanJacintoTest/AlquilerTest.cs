98c08a62192f55276f56439b20b5f9f929b9b600
fbngeldres@gmail.com <fbngeldres@gmail.com@520c7223-f99b-752e-b2e1-b03ac67e0b09>
2014-05-26 04:02:49 +0000

    Se cambio de Nombre a esta clase para que este mas entendible

---
+++ ProyectoX/SanJacintoTest/AlquilerTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SanJacintoTest
+{
+    [TestClass]
+    public class AlquilerTest
+    {
+        [TestMethod]
+        public void registrar()
+        {
+            wsAlquiler.AlquilerServiceClient proxy = new wsAlquiler.AlquilerServiceClient();
+            wsAlquiler.Alquiler alquiler = new wsAlquiler.Alquiler();
+            alquiler.Costo = 1478;
+            alquiler.Accesorios = "RADIO MP4";
+
+            wsAlquiler.Alquiler alquilerNuevo = new wsAlquiler.Alquiler();
+            alquilerNuevo = proxy.registrarAlquiler(alquiler, 1, 1);
+
+            Assert.IsNotNull(alquilerNuevo);
+
+        }
+
+        [TestMethod]
+        public void obtenerAlquiler()
+        {
+            wsAlquiler.AlquilerServiceClient proxy = new wsAlquiler.AlquilerServiceClient();
+
+            wsAlquiler.Alquiler alquilerNuevo = proxy.obtenerAlquiler(2);
+
+            Assert.IsNotNull(alquilerNuevo);
+
+        }
+
+        [TestMethod]
+        public void listarAlquiler()
+        {
+            wsAlquiler.AlquilerServiceClient proxy = new wsAlquiler.AlquilerServiceClient();
+
+            List<wsAlquiler.Alquiler> lista = proxy.listaAlquileres().ToList();
+
+            Assert.IsNotNull(lista);
+
+        }
+    }
+}
