e7ed4baa9ef841faf6d4ac2375ba76ba405eb33c
juniors117 <juniors117@520c7223-f99b-752e-b2e1-b03ac67e0b09>
2014-06-01 18:00:40 +0000

    Creación de la estructura del Proyecto SanJacintoRESTServcices y el Test AutoRESTTest

---
+++ ProyectoX/SanJacintoRESTServices/Dominio/Auto.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Runtime.Serialization;
+
+namespace SanJacintoRESTServices.Dominio
+{
+    [DataContract]
+    public class Auto
+    {
+        [DataMember]
+        public int codigo { get; set; }
+        
+        [DataMember]
+        public int marca { get; set; }
+
+        [DataMember]
+        public int modelo { get; set; }
+
+        [DataMember]
+        public decimal  precio { get; set; }
+
+        [DataMember]
+        public int categoria  { get; set; }
+
+        [DataMember]
+        public int estado { get; set; }
+
+        [DataMember]
+        public string  placa { get; set; }       
+
+        [DataMember]
+        public string  imagen { get; set; }
+        
+
+    }
+}
