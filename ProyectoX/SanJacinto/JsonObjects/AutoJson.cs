45d617520032d79ec53307c2d492b4c9584a59ad
fbngeldres@gmail.com <fbngeldres@gmail.com@520c7223-f99b-752e-b2e1-b03ac67e0b09>
2014-06-03 17:23:38 +0000

    Nuevo Json

---
+++ ProyectoX/SanJacinto/JsonObjects/AutoJson.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SanJacinto.JsonObjects
+{
+    public class AutoJson
+    {
+        public int Codigo { get; set; }
+
+        public Marca Marca { get; set; }
+
+        public Modelo Modelo { get; set; }
+
+        public decimal Precio { get; set; }
+
+        public Categoria Categoria { get; set; }
+
+        public Estado Estado { get; set; }
+
+        public string Placa { get; set; }
+
+        public string Imagen { get; set; }
+    }
+
+    public class Categoria
+    {
+        public int Codigo { get; set; }
+
+        public string Descripcion { get; set; }
+    }
+
+    public class Modelo
+    {
+        public int Codigo { get; set; }
+
+        public string Descripcion { get; set; }
+
+        public Marca Marca { get; set; }
+    }
+
+    public class Marca
+    {
+        public int Codigo { get; set; }
+
+        public string Descripcion { get; set; }
+    }
+
+    public class Estado
+    {
+        public int Codigo { get; set; }
+
+        public string Descripcion { get; set; }
+    }
+}
