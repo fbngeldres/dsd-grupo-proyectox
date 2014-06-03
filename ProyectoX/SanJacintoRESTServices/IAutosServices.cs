762458dfd0fdd6d4a01d093baabb08c4547f5b50
fbngeldres@gmail.com <fbngeldres@gmail.com@520c7223-f99b-752e-b2e1-b03ac67e0b09>
2014-06-03 17:50:57 +0000

    Quitando metodos

--- ProyectoX/SanJacintoRESTServices/IAutosServices.cs
+++ ProyectoX/SanJacintoRESTServices/IAutosServices.cs
@@ -37,12 +37,6 @@ namespace SanJacintoRESTServices
         [WebInvoke(Method = "GET", UriTemplate = "Autos/Marcas", ResponseFormat = WebMessageFormat.Json)]
         List<Marca> ListarMarcas();
 
-        [OperationContract]
-        [WebInvoke(Method = "GET", UriTemplate = "Autos/Categorias", ResponseFormat = WebMessageFormat.Json)]
-        List<Categoria> ListarCategorias();
-
-        [OperationContract]
-        [WebInvoke(Method = "GET", UriTemplate = "Autos/Modelos", ResponseFormat = WebMessageFormat.Json)]
-        List<Modelo> ListarModelos();
+      
     }
 }
